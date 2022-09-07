5f9eb6692928f0019177e05a00d5c52320cd6a9b
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-09-06 21:56:35 -0500

    1.0.2

--- src/component/ContactMe.js
+++ src/component/ContactMe.js
@@ -17,6 +17,11 @@ function ContactMe() {
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Company Name</Form.Label>
+        <Form.Control type="company" placeholder="Enter company name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
